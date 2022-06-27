ddad6e4dc92c40c20cc16b3edd4bba217d322174
Jean-Maxime Lachapelle <JeanMaxime.Lachapelle@officedepot.com>
2022-06-27 17:34:27 -0500

    Changed logging level to INFO

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import base64
 import datetime
+import logging
 import mimetypes
 import os
 from typing import Optional, Any
@@ -134,4 +135,5 @@ def web(path):
 
 
 if __name__ == '__main__':
+    app.logger.setLevel(logging.INFO)
     app.run()
