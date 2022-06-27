a7722a663b2dea257018ac372b26271ec56870fa
Jean-Maxime Lachapelle <JeanMaxime.Lachapelle@officedepot.com>
2022-06-27 12:19:34 -0500

    Added refresh maybe?

--- app.py
+++ app.py
@@ -5,12 +5,24 @@ import mimetypes
 import os
 from typing import Optional, Any
 
+import flask
 from azure.core.credentials import TokenCredential, AccessToken
+from azure.core.exceptions import HttpResponseError
 from azure.storage.blob import BlobServiceClient
 from dateutil import parser
 from flask import Flask, request, redirect, url_for, send_from_directory, stream_with_context, Response
 
-app = Flask(__name__)
+
+class localFlask(Flask):
+
+    def process_response(self, response):
+        response.headers.remove('Server')
+        response.headers['Server'] = os.getenv('APP_SERVER_NAME', 'Unknown')
+        super(localFlask, self).process_response(response)
+        return (response)
+
+
+app = localFlask(__name__)
 
 app_ico = """AAABAAEAEBAAAAEACABoBQAAFgAAACgAAAAQAAAAIAAAAAEACAAAAAAAAAAAAAAAAAAAAAAAAAAA
 AAAAAABjfqAAYIOdAF6ImwBdipoAQJxxAFqPlwBAnXIAW4+YAECecgBAoHQAWJWUAEChdABApHYA
@@ -38,7 +50,8 @@ FRUVFRUVFhYWFhYECBUVFRUVFRUVFhYWFhYWBAQRFRUVFRUVFhYWFhYWFhYEBhAUFRUVFRYWFhYW
 FhYWFhYWBAwVFhYWFhYWFhYWFhYWFhYWFhYWFhYWFv//AAD+fwAA/v8AAP9/AAD+/wAA8A8AAOAP
 AADABwAAwAMAAMADAADgAwAA4AcAAOAPAADwDwAA/j8AAP//AAA="""
 
-BASE='web'
+BASE = 'web'
+
 
 class HeaderToken(TokenCredential):
     def get_token(
@@ -50,7 +63,7 @@ class HeaderToken(TokenCredential):
                 'APP_OAUTH2_TOKEN'),
             expires_on=int(parser.isoparse(request.headers[
                                                'X-Ms-Token-Aad-Expires-On']).timestamp()) if 'X-Ms-Token-Aad-Expires-On' in request.headers else (
-                    datetime.datetime.now().timestamp() + 500))
+                    datetime.datetime.now().timestamp() + 1000))
 
 
 @app.route('/')
@@ -63,19 +76,63 @@ def favicon():
     return Response(response=base64.b64decode(app_ico), status=200, mimetype='image/vnd.microsoft.icon')
 
 
+@app.before_request
+def handle_refresh():
+    _expires = int(parser.isoparse(request.headers['X-Ms-Token-Aad-Expires-On']).timestamp()) \
+        if 'X-Ms-Token-Aad-Expires-On' in request.headers else \
+        (datetime.datetime.now().timestamp() + 1000)
+    if request.path != "/.auth/refresh" and _expires - datetime.datetime.now().timestamp() < 300:
+        return flask.redirect("/.auth/refresh", code=302)
+
+# For debugging
+@app.route(f"/.auth/refresh")
+def refresh():
+    return redirect(url_for(BASE, path='index.html'))
+
 @app.route(f"/{BASE}", defaults={'path': 'index.html'})
 @app.route(f"/{BASE}/<path:path>")
 def web(path):
-    def generate():
-        service = BlobServiceClient(account_url=os.getenv(key='APPLICATION_STORAGE_ACCOUNT'),
-                                    credential=HeaderToken())
-        blob_client = service.get_blob_client(os.getenv(key='APPLICATION_STORAGE_CONTAINER'), path)
+    service = BlobServiceClient(account_url=os.getenv(key='APPLICATION_STORAGE_ACCOUNT'),
+                                credential=HeaderToken())
+    blob_client = service.get_blob_client(os.getenv(key='APPLICATION_STORAGE_CONTAINER'), path)
+    try:
         stream = blob_client.download_blob()
-        for chunk in stream.chunks():
-            yield chunk
+        blob_properties = blob_client.get_blob_properties()
+        mime_type = mimetypes.guess_type(path) or 'text/plain'
+        size = blob_properties.size
+        md5 = None
+        content_encoding = None
+        content_language = None
+        cache_control = None
+        content_disposition = None
+        if blob_properties.content_settings:
+            mime_type = blob_properties.content_settings.content_type
+            cache_control = blob_properties.content_settings.cache_control
+            content_disposition = blob_properties.content_settings.content_disposition
+            content_language = blob_properties.content_settings.content_type
+            md5 = base64.b64encode(blob_properties.content_settings.content_md5).decode('utf-8')
+            content_encoding = blob_properties.content_settings.content_encoding
 
-    mime = mimetypes.guess_type(path)
-    return app.response_class(stream_with_context(generate()), mimetype=mime[0] if mime[0] else 'text/plain')
+        def generate():
+            for chunk in stream.chunks():
+                yield chunk
+
+        r = Response(response=stream_with_context(generate()), status=200,
+                     mimetype=mime_type)
+        r.content_length = size
+        r.content_md5 = md5
+        r.content_encoding = content_encoding
+        r.content_type = mime_type
+        r.content_language = content_language
+        r.last_modified = blob_properties.last_modified
+        if content_disposition:
+            r.headers['Content-Disposition'] = content_disposition
+        if cache_control:
+            r.headers['Cache-Control'] = cache_control
+        return r
+
+    except HttpResponseError as ex:
+        return Response(response=ex.reason, status=ex.status_code, mimetype='text/plain')
 
 
 if __name__ == '__main__':
